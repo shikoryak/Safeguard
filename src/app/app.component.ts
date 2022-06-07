818672e96ae706f3227d12541f5a5388d712393e
shikoryak <shikoryak_oksana@ukr.net>
2022-06-07 11:52:04 +0300

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
-
+interface Benefit {
+  count: string;
+  currency: string;
+  description: string;
+}
+interface Experience {
+  number: string;
+  title: string;
+  description: string;
+}
+interface Work {
+  title: string;
+  description: string;
+  link: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test-design';
+  benefits: Benefit[] = [
+    {
+      count: '3.5',
+      currency: 'Billions',
+      description: 'Preventive measurements implemented (cost reduction, loss of investment).'
+    },
+    {
+      count: '25+',
+      currency: 'Years',
+      description: 'Of experience in minimizing risk and protecting assets.'
+    },
+    {
+      count: '1.5',
+      currency: 'Billions',
+      description: 'Of experience in minimizing risk and protecting assets.'
+    }
+  ]
+  experienceList: Experience[] = [
+    {
+      number: '25+',
+      title: 'years of experience',
+      description: 'In handling legal issues.'
+    },
+    {
+      number: '230',
+      title: 'signature strategies',
+      description: 'Developed for our clients.'
+    },
+    {
+      number: '145',
+      title: 'corporate clients',
+      description: 'We helped thus far.'
+    },
+    {
+      number: '30',
+      title: 'employees working',
+      description: 'For your legal security.'
+    }
+  ]
+
+  ourWorks: Work[] = [
+    {
+      title: 'Safeguarding our <br> clients',
+      description: 'Discretion is key, therefore we have converted all our case studies without mentioning our clients names.',
+      link: 'See Case Studies'
+    },
+    {
+      title: "Let's meet & explore<br>options together",
+      description: "We're here to help and answer any question you might have. We look forward to hearing from you.",
+      link: 'Contack Us'
+    },
+    {
+      title: 'Discover where we<br>came from',
+      description: "We're here to help and answer any question you might have. We look forward to hearing from you.",
+      link: 'Our Timeline'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/header/menu/menu.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
